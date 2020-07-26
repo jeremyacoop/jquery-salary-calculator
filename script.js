30a4eaafa65b5867ad0b9a98dea94394aa6c229a
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2020-07-26 16:57:09 -0500

    jQuery Object Calculator assignment incomplete

--- script.js
+++ script.js
@@ -4,35 +4,50 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('jQuery is ready');
-    $('#employeeSubmit').on('click', console.log('click'));
+    // test submit button
+    $('#employeeSubmit').on('click', function(){console.log('click')});
     $('#employeeSubmit').on('click', getEmployeeInfo());
     }
 
-    empArray = [];
+    employeeArray = [];
     function getEmployeeInfo(){
+        // verify input data
+        if($('#firstName') === ""){
+            alert('First name is required');
+        }else if($('#lastName') === ""){
+            alert('Last name is required');
+        }else if($('#idNumber') === ""){
+            alert('ID is required');
+        }else if($('annualSalary').val() === NaN){
+            alert('Salary must be a number')
+        }
         // collect and store inputs
         let fn = $('#firstName').val();
         let ln = $('#lastName').val();
         let id = $('#idNumber').val();
         let jt = $('#jobTitle').val();
         let as = $('#annualSalary').val();
+        console.log(as);
         // use inputs to build employee object
-        empObject = {
+        employeeObject = {
             nameOne: fn,
             nameTwo: ln,
             ID: id,
             job: jt,
             Salary: as
         }
-        empArray.push(empObject);
-        console.log(empObject);
-        console.log(empArray);
+        employeeArray.push(employeeObject);
+        // console.log(empObject);
+        // console.log(empArray);
         calculateMonthlyCosts();
     }
 
     function calculateMonthlyCosts(employees){
-        let empCosts = 0;
-        for(i=0; i<empArray.length; i++){
-            empCosts += employees[i].as;
+        let employeeCosts = 0;
+        for(i=0; i<employeeArray.length; i++){
+            employeeCosts += employees[i].as;
         }
+        console.log(employeeCosts);
+        monthlyCosts = employees/12;
+        console.log(employees)
     }
