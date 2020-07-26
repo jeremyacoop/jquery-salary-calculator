e7fa88276192921a0f2bdabd8e69471eed560230
jeremyacoop <63517273+jeremyacoop@users.noreply.github.com>
2020-07-26 15:43:31 -0500

    wrote basic getInput and calculateMonthlyCosts

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ $(document).ready(handleReady);
 
 function handleReady() {
     console.log('jQuery is ready');
-    $('#employeeSubmit').on('click', )
+    $('#employeeSubmit').on('click', console.log('click'));
+    $('#employeeSubmit').on('click', getEmployeeInfo());
     }
 
+    empArray = [];
     function getEmployeeInfo(){
         // collect and store inputs
         let fn = $('#firstName').val();
@@ -21,10 +23,16 @@ function handleReady() {
             ID: id,
             job: jt,
             Salary: as
-
         }
+        empArray.push(empObject);
+        console.log(empObject);
+        console.log(empArray);
+        calculateMonthlyCosts();
     }
 
-    function calculateCosts(){
-
+    function calculateMonthlyCosts(employees){
+        let empCosts = 0;
+        for(i=0; i<empArray.length; i++){
+            empCosts += employees[i].as;
+        }
     }
